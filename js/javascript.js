77bb2f66e2b72913004b14372b24b726bf1f611d
Josh West <josh.west@centre.edu>
2020-12-11 16:39:12 -0600

    Fixed fetch.

--- js/javascript.js
+++ js/javascript.js
@@ -4,9 +4,21 @@ var setTitle = function (message)
     .text(message)
     .attr("style", "font-size:60px")
 }
-console.log("Hello World!");
 //SHOUTOUT TO FakeStore API! A great FREE API that provides fake products, carts, and users. Everything you need to create a model store web app.
+//Promise
 fetch('https://fakestoreapi.com/products')
-            //.then(response => response.json())
-            .then(console.log(response => response.text())) //for troubleshooting purposes
+            .then(response => {
+                if (response.ok) 
+                {
+                    console.log("FETCH SUCCESS");
+                    //return response.json();
+                    //maybe put a function call here that updates all the data related parts of the webpage
+                } 
+                else 
+                {
+                    console.log("FETCH NOT SUCCESSFUL");
+                }
+            })
+
             .then(data => console.log(data));
+//setTitle("Welcome to Smalmart!"); //Set title once API loaded
